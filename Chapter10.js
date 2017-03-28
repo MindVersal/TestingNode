cc44fdc4a3b633815be348ebca92c6b1a25e1520
MindVersal <antajchik@gmail.com>
2017-03-28 16:39:50 +0300

    16:40 28.03.2017

--- Chapter10.js
+++ Chapter10.js
@@ -14,4 +14,24 @@ function findDate(string) {
 let stringForDate = "30-1-2003";
 console.log("String: " + stringForDate + " after convert: \n" + findDate(stringForDate));
 console.log();
+console.log("Borobudur".replace(/[ou]/g, "a"));
+console.log("Hopper, Grace\nMcCarthy, John\nRitchie, Dennis".replace(/([\w]+), ([\w]+)/g, "$2 $1"));
+console.log();
+let s = "the cia and fbi";
+console.log(s.replace(/\b(fbi|cia)\b/g, function (str) {
+    return str.toUpperCase();
+}));
+console.log();
+let stock = "1 lemon, 2 cabbages and 101 eggs";
+function minusOne(match, amount, unit) {
+    amount = Number(amount) - 1;
+    if (amount == 1){
+        unit = unit.slice(0, unit.length - 1);
+    } else if (amount == 0){
+        amount = "no";
+    }
+    return amount + " " + unit;
+}
+console.log(stock.replace(/(\d+) (\w+)/g, minusOne));
+console.log();
 console.log("THE END.");
