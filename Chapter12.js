c977d49966562b11dae69e1277b96fe2497645f9
MindVersal <antajchik@gmail.com>
2017-04-10 11:10:03 +0300

    11:11 10.04.2017

--- Chapter12.js
+++ Chapter12.js
@@ -51,5 +51,88 @@ function parse(program) {
     return result.expr;
 }
 console.log(parse("+(-(a, b), /(c, 10))"));
+function evaluate(expr, env) {
+    switch(expr.type){
+        case "value":
+            return expr.value;
+        case "word":
+            if (expr.name in env){
+                return env[expr.name];
+            } else {
+                throw new ReferenceError("Variable is not defined " + expr.name);
+            }
+        case "apply":
+            if (expr.operator.type === "word" && expr.operator.name in specialForms){
+                return specialForms[expr.operator.name](expr.args, env);
+            }
+            let op = evaluate(expr.operator, env);
+            if (typeof op !== "function"){
+                throw new TypeError("Program if not a Function.");
+            }
+            return op.apply(null, expr.args.map(function (arg) {
+                return evaluate(arg, env);
+            }));
+    }
+}
+let specialForms = Object.create(null);
+specialForms["if"] = function (args, env) {
+    if (args.length !== 3){
+        throw new SyntaxError("Wrong count of arguments for If.");
+    }
+    if (evaluate(args[0], env) !== false){
+        return evaluate(args[1], env);
+    } else {
+        return evaluate(args[2], env);
+    }
+};
+specialForms["while"] = function (args, env) {
+    if (args.length !== 2){
+        throw new SyntaxError("Wrong count of arguments for While.");
+    }
+    while (evaluate(args[0], env)){
+        evaluate(args[1], env);
+    }
+    return false;
+};
+specialForms["do"] = function (args, env) {
+    let value = false;
+    args.forEach(function (arg) {
+        value = evaluate(arg, env);
+    });
+    return value;
+};
+specialForms["define"] = function (args, env) {
+    if (args.length !== 2 || args[0].type !== "word"){
+        throw new SyntaxError("Bad use of define.");
+    }
+    let value = evaluate(args[1], env);
+    env[args[0].name] = value;
+    return value;
+};
+let topEnv = Object.create(null);
+topEnv["true"] = true;
+topEnv["false"] = false;
+let prog = parse("if(true, false, true)");
+console.log(evaluate(prog, topEnv));
+["+", "-", "*", "/", "==", "<", ">"].forEach(function (op) {
+    topEnv[op] = new Function("a, b", "return a " + op + "b;");
+});
+topEnv["print"] = function (value) {
+    console.log(value);
+    return value;
+};
+function run() {
+    let env = Object.create(topEnv);
+    let program = Array.prototype.slice.call(arguments, 0).join("\n");
+    return evaluate(parse(program), env);
+}
+run("do(define(total, 0), " +
+    "    define(count, 1)," +
+    "    while(<(count, 11)," +
+    "        do(define(total, +(total, count))," +
+    "            define(count, +(count, 1))))," +
+    "    print(total))");
+console.log();
+
 console.log();
 console.log("THE END.");
