aa3aed7578edc5534d52c494f43e9093bb0f34f8
MindVersal <antajchik@gmail.com>
2017-05-08 14:38:18 +0300

    14:40 08.05.2017

--- Chapter21.js
+++ Chapter21.js
@@ -52,18 +52,34 @@ let http = require("http");
 //     response.end();
 // });
 // server.listen(8000);
+// let request = http.request({
+//     hostname: "eloquentjavascript.net",
+//     path: "/20_node.html",
+//     method: "GET",
+//     headers: {Accept: "text/html"}
+// }, function (response) {
+//     console.log("Service answer with code: ", response.statusCode);
+// });
+// request.end();
+http.createServer(function (request, response) {
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    request.on("data", function (chunk) {
+        response.write(chunk.toString().toUpperCase());
+    });
+    request.on("end", function () {
+        response.end();
+    });
+}).listen(8000);
 let request = http.request({
-    hostname: "eloquentjavascript.net",
-    path: "/20_node.html",
-    method: "GET",
-    headers: {Accept: "text/html"}
+    hostname: "localhost",
+    port: 8000,
+    method: "POST"
 }, function (response) {
-    console.log("Service answer with code: ", response.statusCode);
+    response.on("data", function (chunk) {
+        process.stdout.write(chunk.toString());
+    });
 });
-request.end();
-
-
-
+request.end("Hello server.");
 
 
 
