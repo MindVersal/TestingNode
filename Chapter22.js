a8b0651410a4d093d96f6555f31c16274fa4f309
MindVersal <antajchik@gmail.com>
2017-05-10 11:48:54 +0300

    11:50 10.05.2017

--- Chapter22.js
+++ Chapter22.js
@@ -39,6 +39,83 @@ router.add("DELETE", /^\/talks\/([^\/]+)$/,
     }
     respond(response, 204, null);
 });
+function readStreamAsJSON(stream, callback) {
+    let data = "";
+    stream.on("data", function (chunk) {
+        data += chunk;
+    });
+    stream.on("end", function () {
+        let result, error;
+        try {
+            result = JSON.parse(data);
+        } catch (e){
+            error = e;
+        }
+        callback(error, result);
+    });
+    stream.on("error", function (error) {
+        callback(error);
+    });
+}
+router.add("PUT", /^\/talks\/([^\/]+)$/,
+            function (request, response, title) {
+    readStreamAsJSON(request, function (error, talk) {
+        if (error){
+            respond(response, 400, error.toString());
+        } else if (!talk ||
+                    typeof talk.presenter !== "string" ||
+                    typeof talk.summary !== "string"){
+            respond(response, 400, "Bad talk data.");
+        } else {
+            talk[title] = {title: title,
+                            presenter: talk.presenter,
+                            summary: talk.summary,
+                            comments: []};
+            registerChange(title);
+            respond(response, 204, null);
+        }
+    })
+});
+router.add("POST", /^\/talks\/([^\/]+)$/,
+            function (request, response, title) {
+    readStreamAsJSON(request, function (error, comment) {
+        if (error){
+            respond(response, 400, error.toString());
+        } else if (!comment ||
+                    typeof comment.author !== "string" ||
+                    typeof comment.message !== "string"){
+            respond(response, 400, "Bad comment data.");
+        } else if(title in talks){
+            talks[title].comments.push(comment);
+            registerChange(title);
+            respond(response, 204, null);
+        } else {
+            respond(response, 404, "No talks '" + title + "' found.")
+        }
+    })
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 console.log();
